f945e8773198272add7cb30680d6b5fab8bd16b1
Nicholas Rabow <nicholasrabow@Nicholass-Mac-Studio.local>
2022-06-16 13:31:48 -0600

    get request (tested)
    /item get request endpoint developed using TDD

--- routes/items.js
+++ routes/items.js
@@ -1,17 +1,42 @@
 import express from "express";
+import bodyParser from "body-parser";
 import Toy from "../models/Toy.js";
+const app = express();
 const router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 let items = [new Toy("Car", 1.99), new Toy("Barbie", 5.99)];
+
+function checkAuthentication(token) {
+  if (token == undefined || token.length < 20) {
+    return 401; //unauthorized
+  }
+  return 200; //authorized
+}
 
 router.get("/", (_, res) => {
   res.send("Successful Deploy");
 });
 
 router.get("/item/:name", (req, res) => {
+  let status, body;
+
+  status = checkAuthentication(req.headers.authorization); //check bearer token authorized
+
   const { name } = req.params;
   let item = items.filter((item) => item.name === name)[0];
-  res.json({ ok: true, item });
+  if (status == 401) {
+    body = { error: "User not authorized." };
+  } else if (item == undefined) {
+    status = 404;
+    body = { error: "Item does not exist." };
+  } else {
+    status = 200;
+    body = item;
+  }
+  res.status(status).json(body);
 });
 
 export default router;
--- test/items.js
+++ test/items.js
@@ -1,36 +1,58 @@
 import supertest from "supertest";
 import { assert } from "chai";
+import Toy from "../models/Toy.js";
 
 const request = supertest("localhost:3000");
+const token = "01234567899876543210";
 
 describe("Smoketest", function () {
-  it("Can connect to API", () => {
+  it("Can connect to API", (done) => {
     request.get("/").end((err, res) => {
-      assert.equal(err, null);
       assert.equal(res.text, "Successful Deploy");
+      done();
     });
   });
 });
 
 describe("/item", function () {
-  let item = "Car";
-  let itemPrice = 1.99;
-
   describe("GET", function () {
-    it("Get item that exists", () => {
-      request.get(`/item/${item}`).end((err, res) => {
-        console.log(err);
-        console.log(res.text);
+    let expected = new Toy("Car", 1.99);
+    it("Get item that exists", (done) => {
+      request
+        .get(`/item/${expected.name}`)
+        .set("Authorization", token)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.deepEqual(res.body, expected);
+          done();
+        });
+    });
+
+    it("Get item that does not exist", (done) => {
+      request
+        .get("/item/Lego")
+        .set("Authorization", token)
+        .end((err, res) => {
+          assert.equal(res.status, 404);
+          assert.deepEqual(res.body, { error: "Item does not exist." });
+          done();
+        });
+    });
+
+    it("User not authorized", (done) => {
+      request.get("/item/Car").end((err, res) => {
+        assert.equal(res.status, 401);
+        assert.deepEqual(res.body, { error: "User not authorized." });
+        done();
       });
     });
-    it("Get item that does not exist", () => {});
   });
 
-  describe("POST", function () {
-    it("Post item", () => {});
-  });
-
-  describe("DELETE", function () {
-    it("Delete Item", () => {});
-  });
+  // describe("POST", function () {
+  //   it("Post item", () => {});
+  // });
+  //
+  // describe("DELETE", function () {
+  //   it("Delete Item", () => {});
+  // });
 });
