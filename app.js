5675707e6647d570d3cabec439a97a36c5a96a57
Nicholas Rabow <nicholasrabow@Nicholass-Mac-Studio.local>
2022-06-16 16:07:56 -0600

    post request
    post request implemented with unit tests

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
 import routes from "./routes/items.js";
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
